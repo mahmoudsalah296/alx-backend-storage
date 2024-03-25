d7b942ae054624169328efe05be14c2ee701dab4
Mahmoud Salah <ms685116@gmail.com>
2024-03-25 17:23:27 +0200

    remove name==main

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,8 +2,6 @@
 """MongoDB with python"""
 
 
-if __name__ == "__main__":
-
-    def list_all(mongo_collection):
-        """list all documents in a collection"""
-        return mongo_collection.find()
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    return mongo_collection.find()
