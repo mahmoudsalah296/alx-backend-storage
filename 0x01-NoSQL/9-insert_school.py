b39a268ffb5fd9c3f322ced5c8f90488512a88dd
Mahmoud Salah <ms685116@gmail.com>
2024-03-25 17:32:14 +0200

    insert in mongodb using python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,9 +2,7 @@
 """MongoDB with python"""
 
 
-if __name__ == "__main__":
-
-    def insert_school(mongo_collection, **kwargs):
-        """inserts a new document in a collection based on kwargs"""
-        result = mongo_collection.insert_one({**kwargs})
-        return result.inserted_id
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    result = mongo_collection.insert_one({**kwargs})
+    return result.inserted_id
