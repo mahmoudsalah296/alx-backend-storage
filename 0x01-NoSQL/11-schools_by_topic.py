80c0d5707870565514b2c3a4bc50fa7390c4a72b
Mahmoud Salah <ms685116@gmail.com>
2024-03-25 17:55:15 +0200

    query by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""MongoDB with python"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic:
+    """
+    return mongo_collection.find({"topics": topic})
