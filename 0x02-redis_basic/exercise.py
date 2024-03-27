e8362c86b56c2166de19121f9ca1d14996e555f5
Mahmoud Salah <ms685116@gmail.com>
2024-03-27 17:22:06 +0200

    redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,17 +8,24 @@ types = Union[str, float, int, bytes]
 
 
 class Cache:
-    """Cache Class"""
+    """
+    Cache redis class
+    """
 
-    def __init__(self) -> None:
-        """init method"""
+    def __init__(self):
+        """
+        constructor of the redis model
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: types) -> str:
         """
-        generate a random key using uuid, store the input data in Redis
-        using the random key and return the key
+        generate a random key (e.g. using uuid),
+        store the input data in Redis using the
+        random key and return the key.
+        :param data:
+        :return:
         """
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
