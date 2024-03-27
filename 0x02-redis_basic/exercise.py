2c0568eb785338b11d86357dd1c7e3ef7d1574d2
Mahmoud Salah <ms685116@gmail.com>
2024-03-27 17:15:56 +0200

    redis basics

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""redis basic"""
+import redis
+import uuid
+from typing import Union
+
+types = Union[str, float, int, bytes]
+
+
+class Cache:
+    """Cache Class"""
+
+    def __init__(self) -> None:
+        """init method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: types) -> str:
+        """
+        generate a random key using uuid, store the input data in Redis
+        using the random key and return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
