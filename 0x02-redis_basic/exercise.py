7553a54bb991360f354691b25c6af755c45eb954
Mahmoud Salah <ms685116@gmail.com>
2024-03-27 20:22:46 +0200

    redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,32 +1,103 @@
 #!/usr/bin/env python3
 """redis basic"""
+
+
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+from functools import wraps
 
 types = Union[str, float, int, bytes]
 
 
-class Cache:
-    """
-    Cache redis class
-    """
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of Cache class are called"""
+    key = method.__qualname__
 
-    def __init__(self):
-        """
-        constructor of the redis model
-        """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrap the decorated function and return the wrapper"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs for a particular function"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrap the decorated function and return the wrapper"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
+def replay(fn: Callable):
+    """display the history of calls of a particular function."""
+    r = redis.Redis()
+    func_name = fn.__qualname__
+    c = r.get(func_name)
+    try:
+        c = int(c.decode("utf-8"))
+    except Exception:
+        c = 0
+    print("{} was called {} times:".format(func_name, c))
+    inputs = r.lrange("{}:inputs".format(func_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(func_name), 0, -1)
+    for inp, outp in zip(inputs, outputs):
+        try:
+            inp = inp.decode("utf-8")
+        except Exception:
+            inp = ""
+        try:
+            outp = outp.decode("utf-8")
+        except Exception:
+            outp = ""
+        print("{}(*{}) -> {}".format(func_name, inp, outp))
+
+
+class Cache:
+    """Cache Class"""
+
+    def __init__(self) -> None:
+        """init method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: types) -> str:
         """
-        generate a random key (e.g. using uuid),
-        store the input data in Redis using the
-        random key and return the key.
-        :param data:
-        :return:
+        generate a random key using uuid, store the input data in Redis
+        using the random key and return the key
         """
         key = str(uuid.uuid4())
-        self._redis.mset({key: data})
+        self._redis.set(key, data)
         return key
+
+    def get(self, key, fn):
+        """convert the data back to the desired format"""
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """parametrize Cache.get with correct conversion function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """parametrize Cache.get with correct conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
